b12c75136a8358589edf705c6ed7a5472a88ada2
Escary <81011361+pflyly@users.noreply.github.com>
2025-05-15 07:00:44 +0100

    don't need this

--- eden-macos.sh
+++ eden-macos.sh
@@ -2,9 +2,6 @@
 
 echo "Making Eden for MacOS"
 
-# export Qt6_DIR="/opt/homebrew/opt/qt@6/lib/cmake"
-# export LIBVULKAN_PATH=/opt/homebrew/lib/libvulkan.dylib
-
 if ! git clone 'https://git.eden-emu.dev/eden-emu/eden.git' ./eden; then
 	echo "Using mirror instead..."
 	rm -rf ./eden || true
