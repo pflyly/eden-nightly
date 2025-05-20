9217054bc580c32c0fc6b2d746d4de52983721a6
Escary <81011361+pflyly@users.noreply.github.com>
2025-05-20 13:24:56 +0100

    resolve install path

--- sharun.sh
+++ sharun.sh
@@ -11,28 +11,30 @@ export ARCH="$(uname -m)"
 LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
 
 BUILD_DIR=$(realpath "$1")
-DEPLOY_DIR="${BUILD_DIR}/deploy"
-APPDIR="${BUILD_DIR}/deploy/AppDir"
-
-mkdir -p "${DEPLOY_DIR}"
 cd "${BUILD_DIR}"
 
-# Install base files to AppDir
-DESTDIR="${APPDIR}" ninja install
+# Install the project
+sudo ninja install
 
-cd "${APPDIR}"
+mkdir -p ./AppDir
+cd ./AppDir
 
 cat > eden.desktop << EOL
 [Desktop Entry]
 Type=Application
 Name=Eden nightly
 Icon=eden
+GenericName=Switch Emulator
+Comment=Nintendo Switch video game console emulator
+TryExec=eden
+Exec=eden %f
+Categories=Game;Emulator;Qt;
+MimeType=application/x-nx-nro;application/x-nx-nso;application/x-nx-nsp;application/x-nx-xci;
+Keywords=Nintendo;Switch;
 StartupWMClass=eden
-Exec=eden
-Categories=Game;Emulator;
 EOL
 
-cp -v ./usr/share/icons/hicolor/scalable/apps/org.yuzu_emu.eden.svg ./eden.svg
+cp -v /usr/share/icons/hicolor/scalable/apps/org.yuzu_emu.eden.svg ./eden.svg
 ln -sfv ./eden.svg ./.DirIcon
 
 # Bundle all libs
@@ -41,6 +43,7 @@ wget --retry-connrefused --tries=30 "$LIB4BN" -O ./lib4bin
 chmod +x ./lib4bin
 xvfb-run -a -- ./lib4bin -p -v -e -s -k \
 	/usr/bin/eden \
+ 	$LIBDIR/libyuzu-room.a \
 	$LIBDIR/lib*GL*.so* \
  	$LIBDIR/libSDL2*.so* \
   	$LIBDIR/libSDL3.so* \
@@ -65,8 +68,7 @@ xvfb-run -a -- ./lib4bin -p -v -e -s -k \
 	$LIBDIR/spa-0.2/*/* \
 	$LIBDIR/alsa-lib/*
 
-wget https://github.com/VHSgunzo/sharun/releases/download/v0.6.3/sharun-x86_64 -O sharun
-chmod a+x sharun
-
+# wget https://github.com/VHSgunzo/sharun/releases/download/v0.6.3/sharun-x86_64 -O sharun
+# chmod a+x sharun
 ln -fv ./sharun ./AppRun
 ./sharun -g
