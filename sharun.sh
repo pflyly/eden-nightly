1e57c619ba06b9f36161913230042fd932d9b4bc
Escary <81011361+pflyly@users.noreply.github.com>
2025-05-22 13:19:16 +0100

    build two kinds of appimage

--- sharun.sh
+++ sharun.sh
@@ -13,46 +13,72 @@ LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bi
 BUILD_DIR=$(realpath "$1")
 cd "${BUILD_DIR}"
 
-# Install the project
+# Install eden
 sudo ninja install
 
-mkdir -p ./AppDir
-cd ./AppDir
-
-cp -v /usr/share/applications/eden.desktop ./eden.desktop
-cp -v /usr/share/icons/hicolor/scalable/apps/eden.svg ./eden.svg
-ln -sfv ./eden.svg ./.DirIcon
-
-# Bundle all libs
 LIBDIR="/usr/lib"
-wget --retry-connrefused --tries=30 "$LIB4BN" -O ./lib4bin
-chmod +x ./lib4bin
-xvfb-run -a -- ./lib4bin -p -v -e -s -k \
-	/usr/bin/eden \
- 	$LIBDIR/libyuzu-room.a \
-	$LIBDIR/lib*GL*.so* \
- 	$LIBDIR/libSDL2*.so* \
-  	$LIBDIR/libSDL3.so* \
-	$LIBDIR/dri/* \
-	$LIBDIR/vdpau/* \
-	$LIBDIR/libvulkan* \
-	$LIBDIR/libXss.so* \
-	$LIBDIR/libdecor-0.so* \
-	$LIBDIR/libgamemode.so* \
-	$LIBDIR/qt6/plugins/audio/* \
-	$LIBDIR/qt6/plugins/bearer/* \
-	$LIBDIR/qt6/plugins/imageformats/* \
-	$LIBDIR/qt6/plugins/iconengines/* \
-	$LIBDIR/qt6/plugins/platforms/* \
-	$LIBDIR/qt6/plugins/platformthemes/* \
-	$LIBDIR/qt6/plugins/platforminputcontexts/* \
-	$LIBDIR/qt6/plugins/styles/* \
-	$LIBDIR/qt6/plugins/xcbglintegrations/* \
-	$LIBDIR/qt6/plugins/wayland-*/* \
-	$LIBDIR/pulseaudio/* \
-	$LIBDIR/pipewire-0.3/* \
-	$LIBDIR/spa-0.2/*/* \
-	$LIBDIR/alsa-lib/*
+COMMON_LIBS=(
+    "$LIBDIR/libXss.so*"
+    "$LIBDIR/libgamemode.so*"
+    "$LIBDIR/qt6/plugins/audio/*"
+    "$LIBDIR/qt6/plugins/bearer/*"
+    "$LIBDIR/qt6/plugins/imageformats/*"
+    "$LIBDIR/qt6/plugins/iconengines/*"
+    "$LIBDIR/qt6/plugins/platforms/*"
+    "$LIBDIR/qt6/plugins/platformthemes/*"
+    "$LIBDIR/qt6/plugins/platforminputcontexts/*"
+    "$LIBDIR/qt6/plugins/styles/*"
+    "$LIBDIR/qt6/plugins/xcbglintegrations/*"
+    "$LIBDIR/qt6/plugins/wayland-*/*"
+    "$LIBDIR/pulseaudio/*"
+    "$LIBDIR/spa-0.2/*/*"
+    "$LIBDIR/alsa-lib/*"
+)
 
-ln -fv ./sharun ./AppRun
-./sharun -g
+MESA_EXTRA_LIBS=(
+    "$LIBDIR/lib*GL*.so*"
+    "$LIBDIR/dri/*"
+    "$LIBDIR/vdpau/*"
+    "$LIBDIR/libvulkan*"
+    "$LIBDIR/libdecor-0.so*"
+)
+
+# Create a build function to handle two kinds of appimage
+build_appimage() {
+    local build_type="$1"
+    local lib4bin_flags=("${!2}")
+    local extra_libs=("${!3}")
+    local appdir="./$build_type/AppDir"
+
+    echo "=== Building $build_type AppImage ==="
+    mkdir -p "$appdir"
+    cd "$appdir"
+
+    cp -v /usr/share/applications/eden.desktop ./eden.desktop
+    cp -v /usr/share/icons/hicolor/scalable/apps/eden.svg ./eden.svg
+    ln -sfv ./eden.svg ./.DirIcon
+
+    wget --retry-connrefused --tries=30 "$LIB4BN" -O ./lib4bin
+    chmod +x ./lib4bin
+
+    ./lib4bin "${lib4bin_flags[@]}" \
+        /usr/bin/eden \
+        "${COMMON_LIBS[@]}" \
+        "${extra_libs[@]}"
+
+    ln -fv ./sharun ./AppRun
+    ./sharun -g
+
+    cd - > /dev/null
+}
+
+# set lib4bin flags
+MESA_FLAGS=(-p -v -e -s -k)
+LIGHT_FLAGS=(-p -v -s -k)
+
+# Build Appimage with mesa drivers for maximum compatibility and possible latest fixes
+build_appimage "mesa" MESA_FLAGS[@] MESA_EXTRA_LIBS[@]
+
+# Build Appimage without mesa drivers for lightweight
+EMPTY=()
+build_appimage "light" LIGHT_FLAGS[@] EMPTY[@]
