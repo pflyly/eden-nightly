061aefa3447dd217a29f5fae7334544c2096be08
Escary <81011361+pflyly@users.noreply.github.com>
2025-05-22 20:54:43 +0100

    try to hanlde frequent failure of submodule update

--- eden-appimage.sh
+++ eden-appimage.sh
@@ -53,14 +53,32 @@ esac
 
 UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
 
-# Clone Eden, fallback to mirror if upstream repo fails to clone
-if ! git clone 'https://git.eden-emu.dev/eden-emu/eden.git' ./eden; then
-	echo "Using mirror instead..."
-	rm -rf ./eden || true
-	git clone 'https://github.com/pflyly/eden-mirror.git' ./eden
-fi
+# We really need to hanlde this due to frequent failure of submodule update
+clone_eden() {
+	# Clone Eden, fallback to mirror if upstream repo fails to clone
+	if ! git clone 'https://git.eden-emu.dev/eden-emu/eden.git' ./eden; then
+		echo "Using mirror instead..."
+		rm -rf ./eden || true
+		git clone 'https://github.com/pflyly/eden-mirror.git' ./eden
+	fi
+}
 
+rm -rf ./eden || true
+clone_eden
 cd ./eden
+
+if ! git submodule update --init --recursive; then
+    echo "Submodule update failed! Deleting and re-cloning the entire repo."
+    
+    # Re-cloning the entire repo in case of submodule corruption
+    cd ..
+    rm -rf ./eden || true
+    clone_eden
+    cd ./eden
+
+    # Now try submodules again — if this fails again, let it explode!!!
+    git submodule update --init --recursive
+fi
 
 # Get current commit info
 DATE="$(date +"%Y%m%d")"
@@ -93,8 +111,6 @@ echo "Full Changelog: [\`${RELEASE_TAG}...master\`](${BASE_COMPARE_URL}/${RELEAS
 if [ "$1" = 'aarch64' ]; then
     sed -i 's/Settings::values\.lru_cache_enabled\.GetValue()/true/' src/core/arm/nce/patcher.h
 fi
-
-git submodule update --init --recursive
 
 mkdir build
 cd build
